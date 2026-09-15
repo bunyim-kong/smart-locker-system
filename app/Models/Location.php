d30f60d7e56227bb67eaffdb4a09920f421aba09
theasithul <theasithul9@gmail.com>
2026-09-15 14:48:22 +0700

    do model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Location extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'address',
+        'map_link',
+    ];
+    // public function lockers(): HasMany
+
+    // {
+    //     return $this-> hasMany(locker::class) 
+    //         ->orderBy('title');
+    // }
 }
