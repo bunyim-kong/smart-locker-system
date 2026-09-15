5927b6bf9f8d0576a976c72deb3c512642408324
theasithul <theasithul9@gmail.com>
2026-09-15 14:48:22 +0700

    do model

--- app/Models/History.php
+++ app/Models/History.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class History extends Model
 {
-    //
+    protected $fillable = [
+        'locker_id',
+        'user_id',
+        'start_time',
+        'end_time',
+    ];
+    // public function lockers(): HasMany
+
+    // {
+    //     return $this-> hasMany(Locker::class) 
+    //         ->orderBy('title');
+    // }
 }
