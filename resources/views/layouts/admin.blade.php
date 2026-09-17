e4d29360839bbb55a25e97af1d61aa74d355f1ab
yim <bunyimkong@gmail.com>
2026-09-17 09:31:35 +0700

    feat: finished all layout by bunyim

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Smart Locker')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components/sidebar.css') }}">
 </head>
 <body>
     @include('admin.partial.sidebar')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Smart Locker')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components/footer.css') }}">
 </head>
 <body>
+    @include('front.partials.navbar')
+
+    <main>
+        @yield('content');
+    </main>
+
     
+    @include('front.partials.footer')
 </body>
 </html>
