825f8742d281f356d1de0d30d442f8eb3f341d32
yim <bunyimkong@gmail.com>
2026-09-18 08:00:27 +0700

    .

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@include('layouts.admin')
-
-@section('title', 'Admin Smart Locker')
-
-@section('content')
-    <section>
-        lkkadjflaksdfffffffffffffffffffffffffksadffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-    </section>
-@endsection
